f72370c5c81e3990eaf489d5d0fc617af8f8a8aa
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-05-18 17:27:21 -0300

    authentication middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,24 @@
 import dotenv from "dotenv"
 
-export async function authValidate(req, res, next){
-    const {Authorization} = req.headers
+export async function authValidate(req, res, next) {
+    const { Authorization } = req.headers
     const token = Authorization?.replace("Bearer ", "")
     if (!token) return res.sendStatus(401)
 
     dotenv.config()
     const secretKey = process.env.SECRET_KEY
 
-    try{
+    try {
         const data = await jwt.verify(token, secretKey)
-        if(!data) return res.sendStatus(401)
-        const session = await db.query(`SELECT * FROM sessions WHERE `)
+        if (!data) return res.status(401).send("Token invalido")
 
-    }catch(err){
+        const session = await db.query(`SELECT * FROM sessions WHERE "userId=$1`, [data.id])
+        if (!session) return res.status(401).send("Sessao nao encontrada")
+        res.locals.session = session
+
+        next()
+
+    } catch (err) {
         res.status(500).json(err.message);
     }
 }
